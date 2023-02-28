750a457f6e8f6696bbe7b9eb2db339a7b2d95c10
Kshitiz <kshitiz@192.168.1.2>
2023-03-01 03:28:25 +0530

    feat(add-docker-support): add docker files and update constants

--- user-client/src/helper/Const.js
+++ user-client/src/helper/Const.js
@@ -1,8 +1,9 @@
-export const HOST_URL =
-    process.env.NODE_ENV === "production" ? process.env.REACT_APP_HOST : "http://localhost:8080";
+export const HOST_URL = process.env.REACT_APP_HOST
+  ? process.env.REACT_APP_HOST
+  : "http://localhost:8080";
 
 export const UPLOAD_TYPE =
-    process.env.NODE_ENV === "production" ? "cloud" : "host";
+  process.env.CLIENT_UPLOAD_TYPE === "cloud" ? "cloud" : "host";
 
 export const UPLOAD_SUPPORT =
-    process.env.NODE_ENV === "production" ? process.env.REACT_APP_UPLOADS : "enabled";
+  process.env.REACT_APP_UPLOADS === "enabled" ? "enabled" : "disabled";
